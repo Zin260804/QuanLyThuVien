34dbf96c44ac6c489c96993d1ac6bcaf396d4282
Zin260804 <nguyenhuuvinh3701@gmail.com>
2024-10-22 13:23:51 +0700

    first

---
+++ QLSach/FCuonSachViTri.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace QuanLyThuVien.ThuThu
+{
+    partial class FCuonSachViTri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gunaLabel5 = new Guna.UI.WinForms.GunaLabel();
+            this.btnTimKiem = new Guna.UI.WinForms.GunaButton();
+            this.dtCuonSachViTri = new Guna.UI.WinForms.GunaDataGridView();
+            this.txtTimKiem = new Guna.UI2.WinForms.Guna2TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtCuonSachViTri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtTimKiem);
+            this.panel1.Controls.Add(this.gunaLabel5);
+            this.panel1.Controls.Add(this.btnTimKiem);
+            this.panel1.Location = new System.Drawing.Point(31, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(944, 101);
+            this.panel1.TabIndex = 21;
+            // 
+            // gunaLabel5
+            // 
+            this.gunaLabel5.AutoSize = true;
+            this.gunaLabel5.Font = new System.Drawing.Font("Segoe UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(188)))), ((int)(((byte)(167)))));
+            this.gunaLabel5.Location = new System.Drawing.Point(17, 0);
+            this.gunaLabel5.Name = "gunaLabel5";
+            this.gunaLabel5.Size = new System.Drawing.Size(288, 31);
+            this.gunaLabel5.TabIndex = 23;
+            this.gunaLabel5.Text = "Thông tin vị trí cuốn sách";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.AnimationHoverSpeed = 0.07F;
+            this.btnTimKiem.AnimationSpeed = 0.03F;
+            this.btnTimKiem.BackColor = System.Drawing.Color.Transparent;
+            this.btnTimKiem.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(188)))), ((int)(((byte)(167)))));
+            this.btnTimKiem.BorderColor = System.Drawing.Color.Black;
+            this.btnTimKiem.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnTimKiem.FocusedColor = System.Drawing.Color.Empty;
+            this.btnTimKiem.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.ForeColor = System.Drawing.Color.White;
+            this.btnTimKiem.Image = global::QuanLyThuVien.Properties.Resources.find;
+            this.btnTimKiem.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnTimKiem.ImageSize = new System.Drawing.Size(30, 30);
+            this.btnTimKiem.Location = new System.Drawing.Point(418, 54);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.OnHoverBaseColor = System.Drawing.Color.Goldenrod;
+            this.btnTimKiem.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnTimKiem.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnTimKiem.OnHoverImage = null;
+            this.btnTimKiem.OnPressedColor = System.Drawing.Color.Black;
+            this.btnTimKiem.Radius = 10;
+            this.btnTimKiem.Size = new System.Drawing.Size(48, 32);
+            this.btnTimKiem.TabIndex = 21;
+            this.btnTimKiem.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dtCuonSachViTri
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dtCuonSachViTri.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dtCuonSachViTri.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtCuonSachViTri.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dtCuonSachViTri.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtCuonSachViTri.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dtCuonSachViTri.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(188)))), ((int)(((byte)(167)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlDark;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dtCuonSachViTri.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dtCuonSachViTri.ColumnHeadersHeight = 20;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtCuonSachViTri.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dtCuonSachViTri.EnableHeadersVisualStyles = false;
+            this.dtCuonSachViTri.GridColor = System.Drawing.Color.White;
+            this.dtCuonSachViTri.Location = new System.Drawing.Point(31, 130);
+            this.dtCuonSachViTri.Name = "dtCuonSachViTri";
+            this.dtCuonSachViTri.RowHeadersVisible = false;
+            this.dtCuonSachViTri.RowHeadersWidth = 51;
+            this.dtCuonSachViTri.RowTemplate.Height = 24;
+            this.dtCuonSachViTri.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtCuonSachViTri.Size = new System.Drawing.Size(944, 642);
+            this.dtCuonSachViTri.TabIndex = 20;
+            this.dtCuonSachViTri.Theme = Guna.UI.WinForms.GunaDataGridViewPresetThemes.Guna;
+            this.dtCuonSachViTri.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dtCuonSachViTri.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dtCuonSachViTri.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dtCuonSachViTri.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dtCuonSachViTri.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dtCuonSachViTri.ThemeStyle.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.dtCuonSachViTri.ThemeStyle.GridColor = System.Drawing.Color.White;
+            this.dtCuonSachViTri.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(188)))), ((int)(((byte)(167)))));
+            this.dtCuonSachViTri.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dtCuonSachViTri.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtCuonSachViTri.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.DimGray;
+            this.dtCuonSachViTri.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dtCuonSachViTri.ThemeStyle.HeaderStyle.Height = 20;
+            this.dtCuonSachViTri.ThemeStyle.ReadOnly = false;
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.Height = 24;
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.SystemColors.Control;
+            this.dtCuonSachViTri.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.BorderRadius = 10;
+            this.txtTimKiem.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtTimKiem.DefaultText = "";
+            this.txtTimKiem.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtTimKiem.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtTimKiem.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTimKiem.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTimKiem.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTimKiem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtTimKiem.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTimKiem.Location = new System.Drawing.Point(23, 54);
+            this.txtTimKiem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.PasswordChar = '\0';
+            this.txtTimKiem.PlaceholderText = "Tìm kiếm";
+            this.txtTimKiem.SelectedText = "";
+            this.txtTimKiem.Size = new System.Drawing.Size(389, 32);
+            this.txtTimKiem.TabIndex = 37;
+            // 
+            // FCuonSachViTri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(988, 784);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dtCuonSachViTri);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FCuonSachViTri";
+            this.Text = "FCuonSachViTri";
+            this.Load += new System.EventHandler(this.FCuonSachViTri_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtCuonSachViTri)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel5;
+        private Guna.UI.WinForms.GunaButton btnTimKiem;
+        private Guna.UI.WinForms.GunaDataGridView dtCuonSachViTri;
+        private Guna.UI2.WinForms.Guna2TextBox txtTimKiem;
+    }
+}
---
+++ QLSach/FDauSach.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyThuVien.DAL;
+using QuanLyThuVien.DTO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace QuanLyThuVien.ThuThu
+{
+   
+    public partial class FDauSach : Form
+    {
+        DauSachDao dao= new DauSachDao();
+        public FDauSach()
+        {
+            InitializeComponent();         
+            dtDauSach.DataSource = dao.LoadDauSach(); 
+        }
+        public void LoadData()
+        {
+            dtDauSach.DataSource = dao.LoadDauSach();
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            DauSach dausachmoi = new DauSach(txtMaDS.Text,txtMaTacGia.Text,
+                txtMaNhaXuatBan.Text,txtTen.Text,txtTheLoai.Text);
+            dao.ThemDauSach(dausachmoi);
+            dtDauSach.DataSource = dao.LoadDauSach();
+        }
+
+        private void dtDauSach_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow selectedRow = dtDauSach.Rows[e.RowIndex];
+            txtMaDS.Text = selectedRow.Cells["MaDS"].Value.ToString();
+            txtTen.Text = selectedRow.Cells["Ten"].Value.ToString();
+            txtMaTacGia.Text = selectedRow.Cells["MaTG"].Value.ToString();
+            txtMaNhaXuatBan.Text = selectedRow.Cells["MaNXB"].Value.ToString();
+            txtTheLoai.Text = selectedRow.Cells["TheLoai"].Value.ToString();
+    
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DauSach dausachmoi = new DauSach(txtMaDS.Text, txtMaTacGia.Text,
+                 txtMaNhaXuatBan.Text, txtTen.Text, txtTheLoai.Text);
+            dao.XoaDauSach(dausachmoi);
+            dtDauSach.DataSource = dao.LoadDauSach();
+        }
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            DauSach dausachmoi = new DauSach(txtMaDS.Text, txtMaTacGia.Text,
+              txtMaNhaXuatBan.Text, txtTen.Text, txtTheLoai.Text);
+            dao.SuaDauSach(dausachmoi);
+            dtDauSach.DataSource = dao.LoadDauSach();
+        }
+
+        private void txtTimKiem_KeyUp(object sender, KeyEventArgs e)
+        {
+            string keyword = txtTimKiem.Text.Trim();     
+            dao.TimKiemDauSach(keyword, dtDauSach);
+        }
+    }
+}
